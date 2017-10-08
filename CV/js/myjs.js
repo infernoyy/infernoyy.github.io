34873ad3683755d766530516ac92f53339335657
infernoyy <1085435176@qq.com>
2017-10-08 16:49:52 +0800

    Site updated: 2017-10-08 16:49:52

--- CV/js/myjs.js
+++ CV/js/myjs.js
@@ -1,19 +1,4 @@
 window.onload=function(){
 	var oli =document.getElementsByTagName("li");
 	var box = document.getElementsByClassName("box")[0];
-	for(var i= 0;i<oli.length;i++){
-		oli[i].style.backgroundPositionX=-230*i+"px";
-	}
-	for(var i= 0;i<oli.length;i++){
-		oli[i].onmouseover = function(){
-			var _left = this.offsetLeft;
-			var _top = this.offsetTop;
-			box.style.left = _left+"px";
-			box.style.top = _top+"px";
-			this.style.backgroundPositionY="-150px";   //字向上滚动150px
-		}
-		oli[i].onmouseout = function(){
-			this.style.backgroundPositionY="0px"    //返回原来样子
-		}
-	}
-}
+	for(var i= 0;i
